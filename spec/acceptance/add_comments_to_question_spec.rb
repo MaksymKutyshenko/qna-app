317cf8fa1e838629462c05043bfcc86a2886e20c
Maksym Kutyshenko <mkutyshenko@gmail.com>
2016-10-23 13:53:15 +0300

    only registered user can add comment

--- spec/acceptance/add_comments_to_question_spec.rb
+++ spec/acceptance/add_comments_to_question_spec.rb
@@ -10,12 +10,34 @@ feature 'Add comments', %q{
   given!(:question) { create(:question) }
 
   describe 'Authenticated user' do 
+    before { sign_in(user) }
+
     scenario 'adds comment to question', js: true do 
       visit question_path(question)
       within '.question .comments' do 
         fill_in 'Comment text', with: 'Question comment'
         click_on 'Add comment'
         expect(page).to have_content 'Question comment'
+      end
+      expect(page).to have_content 'Your comment cussessfully created'
+    end
+
+    scenario 'adds comment to question with wrong params', js: true do 
+      visit question_path(question)
+      within '.question .comments' do 
+        fill_in 'Comment text', with: ''
+        click_on 'Add comment'
+        expect(page).to_not have_css '.comment'
+      end
+      expect(page).to have_content 'Body can\'t be blank'
+    end 
+  end
+
+  describe 'Non-authenticated user' do 
+    scenario 'adds comment to question', js: true do 
+      visit question_path(question)
+      within '.question .comments' do 
+        expect(page).to_not have_css '.new_comment'
       end
     end 
   end
