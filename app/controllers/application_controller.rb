43e502baced614b9b557044d354b2423d10414aa
Maksym Kutyshenko <mkutyshenko@gmail.com>
2016-10-25 11:55:12 +0300

    Some fixes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,6 @@ class ApplicationController < ActionController::Base
 
   private
   def gon_user
-    gon.user_id = current_user ? current_user.id : nil
+    gon.user_id = current_user.try(:id)
   end
 end
