1c9ef5ae989bc9119f4d845a24cdad9948f9c3ae
Maksym Kutyshenko <mkutyshenko@gmail.com>
2016-10-30 18:24:27 +0200

    auth fixes

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,9 +1,20 @@
 class OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def facebook
-    @user = User.find_for_oauth(request.env['omniauth.auth'])
+    @user = User.find_for_oauth(auth_hash)
     if @user.persisted?
       sign_in_and_redirect @user, event: :authentication
       set_flash_message(:notice, :success, kind: 'Facebook') if is_navigational_format?
     end    
   end
+
+  def twitter
+    render json: auth_hash
+  end
+
+  private
+
+  def auth_hash
+    request.env['omniauth.auth']
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, 
-         :validatable, :omniauthable, omniauth_providers: [:facebook, :tweeter]
+         :validatable, :omniauthable, omniauth_providers: [:facebook, :twitter]
   has_many :questions, dependent: :destroy
   has_many :answers, dependent: :destroy
   has_many :votes, dependent: :destroy
